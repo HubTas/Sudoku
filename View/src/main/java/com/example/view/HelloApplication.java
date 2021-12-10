2f6d4aae245b2a306c8786f1290ed23ce99d57c1
236602 Mielczarek Jakub <236602@edu.p.lodz.pl>
2021-12-10 16:38:38 +0100

    Dodany formularz poziomow

--- View/src/main/java/com/example/view/HelloApplication.java
+++ View/src/main/java/com/example/view/HelloApplication.java
@@ -3,18 +3,23 @@ package com.example.view;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) throws Exception {
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(this.getClass().getResource("board.fxml"));
+    Pane pane = loader.load();
+    Scene scene = new Scene(pane);
+
+     stage.setTitle("Sudoku Game");
+     stage.show();
+     stage.setScene(scene);
     }
 
     public static void main(String[] args) {
--- View/src/main/java/com/example/view/HelloController.java
+++ View/src/main/java/com/example/view/HelloController.java
@@ -1,14 +1,31 @@
 package com.example.view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
-    private Label welcomeText;
+    private ComboBox SudokuDifficulty;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label label1;
+
+    @FXML
+    void selectLevel(ActionEvent event) {
+        String s = SudokuDifficulty.getSelectionModel().getSelectedItem().toString();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<String> list = FXCollections.observableArrayList("Easy","Medium","Hard");
+        SudokuDifficulty.setItems(list);
     }
 }
